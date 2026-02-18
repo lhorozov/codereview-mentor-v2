3d7544e7dce932d334745611fabf594255848b92
Lyuben Horozov <lhorozov@yahoo.com>
2026-02-18 21:44:14 +0200

    init: add tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import type { Metadata } from 'next'
+import './globals.css'
+ 
+export const metadata: Metadata = {
+  title: 'Code Review Mentor',
+  description: 'The best AI code review tool',
+}
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <h1>CodeReview Mentor</h1>
+  return <main className="flex flex-col items-center p-6 bg-zinc-200 text-zinc-700">
+    <h1 className="text-4xl font-bold">Code Review Mentor</h1>
+  </main>
 }
