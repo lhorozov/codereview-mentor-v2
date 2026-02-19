b95100e8b69dc5ba90702bf2e4a9f796fb0dd960
Lyuben Horozov <lhorozov@yahoo.com>
2026-02-19 21:57:41 +0200

    add components to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client"
 
+import CodeEditor from "@/components/code-editor";
+import { SelectLang } from "@/components/select-lang";
 import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return <main className="flex flex-col items-center p-6 bg-zinc-200 text-zinc-700">
     <h1 className="text-4xl font-bold">Code Review Mentor</h1>
-    <Button variant="outline" onClick={() => null}>Button</Button>
+    <div className='m-6'>
+      <SelectLang />
+    </div>
+    <CodeEditor />
+    <div className='pt-6'>
+      <Button className='active:scale-[0.96]'>
+        Review code
+      </Button>
+    </div>
   </main>
 }
